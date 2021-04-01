428465dd293cafdb8fb872ad8759936c18aa878b
Matt Wrock <matt@mattwrock.com>
2021-04-01 14:40:31 -0700

    delete iconv library aquired via bash dep
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- config/software/mac-bootstrapper.rb
+++ config/software/mac-bootstrapper.rb
@@ -18,3 +18,7 @@ dependency "jq"
 
 # Libcharset is safe to rely on as it's part of OSX
 whitelist_file /libarchive\..+/
+
+# this gets pulled in as a bash dependency but
+# we do not want it in the final lib
+delete "#{install_dir}/embedded/lib/libiconv.*"
